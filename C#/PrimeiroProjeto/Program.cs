aafb833242d47fedfac7f6020e7f44de9d1c7a7d
Bruno Neemias <89363512+brunoneemias@users.noreply.github.com>
2023-02-11 09:22:04 -0300

    Add files via upload

---
+++ C#/PrimeiroProjeto/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 24;
+            string nome = "Bruno Neemias";
+            bool ativo = false;
+            decimal salario = 5840.99m;
+            float descontos = 1200.99f;
+
+      
+   
+            Console.WriteLine($"O usuario {nome} tem {idade} anos de idade, esta {ativo} no sistema e ganha {salario}");
+            Console.WriteLine("---------------------------------------------------------------------------------------");
+            dynamic x = "Essa variavel é uma string";
+            Console.WriteLine(x);
+            Console.WriteLine("---------------------------------------------------------------------------------------");
+            x = 10;
+            Console.WriteLine(x);
+            x = true;
+            Console.WriteLine(x);
+        }
+    } 
+}
+
