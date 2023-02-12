7a260037de95fb939d234b8ea2b724cdcd4fe4d4
Bruno Neemias <89363512+brunoneemias@users.noreply.github.com>
2023-02-12 18:41:58 -0300

    Add files via upload

---
+++ C#/5.Filas/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Queue fila = new Queue();
+        fila.Enqueue("Bruno");
+        fila.Enqueue("Amanda");
+        fila.Enqueue("Tony");
+        Console.WriteLine(fila.Peek());
+        Console.WriteLine($"A fila tem {fila.Count} itens");
+        Console.WriteLine($"O item {fila.Dequeue()} foi retirado");
+        Console.WriteLine($"A fila tem {fila.Count} itens");
+    }
+}
---
+++ C#/6.Tuplas/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Security;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        (string, string, double) brasil = ("Brasil", "Brasilia", 1268.11);
+        Console.WriteLine($"O {brasil.Item1}, cuja sua capital é {brasil.Item2} tem rpc de R$ {brasil.Item3}");
+    }
+}
