883aef63ad1c23314e5477d7e3f4cc41aeb00248
Bruno Neemias <89363512+brunoneemias@users.noreply.github.com>
2023-02-12 18:21:54 -0300

    Add files via upload

---
+++ C#/3.Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] x = new int[3];
+        x[0] = 5;
+        x[1] = 6;
+        x[2] = 7;   
+
+        Console.WriteLine(1);
+        Console.WriteLine(2);  
+        Console.WriteLine(3);
+
+        string[] nomes = new string[3];
+        nomes[0] = "Tainha";
+        nomes[1] = "Vinho";
+        nomes[2] = "Sexo";
+
+        Console.WriteLine(nomes[0]);
+        Console.WriteLine(nomes[1]);
+        Console.WriteLine(nomes[2]);
+
+        string[] diasSemanas = new string[] { "Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab" };
+        Console.WriteLine(diasSemanas[2]);
+    }
+}
---
+++ C#/4.Pilhas/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Stack<int> p1 = new Stack<int>(); 
+        p1.Push(1);
+        p1.Push(2);
+        p1.Push(3);
+        p1.Push(4);
+        p1.Push(5);
+        p1.Push(6);
+        p1.Push(7);
+        p1.Push(8);
+        p1.Push(9);
+         
+        Console.WriteLine($"A pilha tem {p1.Count} itens.");
+        Console.WriteLine(p1.ToArray().GetValue(5)); 
+        p1.Pop();
+        Console.WriteLine(p1.Peek());
+        p1.Clear();   
+    }
+}
