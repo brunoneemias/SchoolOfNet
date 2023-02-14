fc11bc8f31b68b971484c8be7380e751bc4e3565
Bruno Neemias <89363512+brunoneemias@users.noreply.github.com>
2023-02-13 22:52:06 -0300

    Add files via upload

---
+++ C#/7.if e Else/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection.Metadata;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double gastosmensais, salario = 0;
+        bool decimo = false; 
+
+        Console.WriteLine("Informe seu salário: "); 
+        salario = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Informe seus gastos mensais: "); 
+        gastosmensais = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Tem décimo terceiro? ");
+        Boolean.TryParse(Console.ReadLine(), out decimo);   
+
+        if (decimo)
+        {
+            salario += salario; 
+        }
+
+        if (gastosmensais > salario)
+        {
+            Console.WriteLine("Precisa economizar!");
+
+        }
+        else
+        {
+            Console.WriteLine("Legal! Sobrou dinheiro para comprar BTC!!!");
+           
+        }
+
+    }
+}
